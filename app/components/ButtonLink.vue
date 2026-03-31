12453c303cfc2d670d64b3fb2462397efa9bca3c
tehnarter <tehnarter@gmail.com>
2026-03-31 10:49:14 +0300

    feat(components): add ButtonLink.vue

---
+++ app/components/ButtonLink.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const localePath = useLocalePath()
+
+const props = defineProps<{
+  viewAllLink?: string
+}>()
+</script>
+
+<template>
+  <NuxtLink :to="localePath(props.viewAllLink || '/')" class="view-all-btn">
+    <slot />
+  </NuxtLink>
+</template>
+<style scoped lang="scss">
+.view-all-btn {
+  display: inline-block;
+  padding: 14px 42px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  background: rgb(177, 173, 173);
+  border-radius: 9999px;
+  font-size: 15px;
+  font-weight: 500;
+  transition: 0.5s ease;
+
+  &:hover {
+    background: black;
+    color: white;
+  }
+}
+</style>
