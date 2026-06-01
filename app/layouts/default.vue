9cdb440ec91b244771b7e0025095a414c033d882
tehnarter <tehnarter@gmail.com>
2026-06-01 14:53:07 +0300

    refactor(layouts) optimized default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,23 @@
+<script setup lang="ts">
+const isPolicyOpen = ref(false)
+</script>
 <template>
   <div class="layout">
+    <NuxtLoadingIndicator
+      :throttle="0"
+      color="#9ca0a6"
+      error-color="#e53935"
+      :height="3"
+      :duration="2000"
+    />
     <div class="wrapper">
+      <Header />
+
       <NuxtPage />
+      <Footer @open-policy="isPolicyOpen = true" />
+      <Teleport to="body">
+        <PolicyApp v-if="isPolicyOpen" @close="isPolicyOpen = false" />
+      </Teleport>
     </div>
   </div>
 </template>
